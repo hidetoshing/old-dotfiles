8d67244a370770b031a6e8a99ad5204e80e3d955
hidetoshing <hidetoshing@gmail.com>
2015-11-12 01:05:16 +0900

    [fix] install path

--- init/autojump.sh
+++ init/autojump.sh
@@ -5,6 +5,6 @@ freebsd*|darwin*)
 	brew install autojump
     ;;
 linux*)
-    cd ${HOME}/local/bin && git clone https://github.com/joelthelion/autojump.git && cd autojump $$ ./install.py
+    cd ${HOME}/local/src && git clone https://github.com/joelthelion/autojump.git && cd autojump $$ ./install.py
     ;;
 esac
